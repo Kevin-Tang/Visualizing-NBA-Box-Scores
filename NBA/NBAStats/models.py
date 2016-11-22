aa74be6b618cb83ad05fde01eb8dc1fcf49edb33
Kevin Tang <ctang@macalester.edu>
2016-11-22 14:47:01 -0600

    created superusers for django, modified database

--- NBA/NBAStats/models.py
+++ NBA/NBAStats/models.py
@@ -9,8 +9,20 @@ class Team(models.Model):
 	winsLosses = models.CharField(max_length=10)
 	teamName = models.CharField(max_length=50, unique=True)
 
+	def __str__(self):
+		return 'Team: %s' % (self.teamName)
+
 class Game(models.Model):
-	gameDate = models.DateTimeField('Date of Game')
 	gameID = models.CharField(max_length=50, unique=True)
 	firstTeam = models.ForeignKey(Team, related_name='firstTeam', on_delete=models.CASCADE)
 	secondTeam = models.ForeignKey(Team, related_name='secondTeam', on_delete=models.CASCADE)
+
+	def __str__(self):
+		return 'Game ID: %s' % (self.gameID)
+
+class Date(models.Model):
+	date = models.DateTimeField("Date")
+	gamesOfDate = models.ForeignKey(Game, related_name='gamesOfDate', on_delete=models.CASCADE)
+
+	def __str__(self):
+		return 'Date: %s' % (self.date)
