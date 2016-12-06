2754211e3c26d2dc29e63f6fab1cc8ee9f87eefa
HyunUk Jeong <hjeong326@gmail.com>
2016-12-06 16:11:45 -0600

    Trying shotchart in anotherExample.py

--- nba_py-master/examples/ourExample.py
+++ nba_py-master/examples/ourExample.py
@@ -3,15 +3,15 @@ from nba_py import game, shotchart
 gameID = '0041400122'
 
 def boxScore():
-    # boxscore = game.Boxscore(gameID)
-    # print(boxscore.team_stats())
+    boxscore = game.Boxscore(gameID)
+    print(boxscore.team_stats())
 
     # advBoxscore = game.BoxscoreAdvanced(gameID)
     # print(advBoxscore.team_stats())
 
     # print(game.BoxscoreSummary(gameID).game_info())
-    #print(game.BoxscoreSummary(gameID).game_summary())
-    #print(game.BoxscoreSummary(gameID).line_score())
+    # print(game.BoxscoreSummary(gameID).game_summary())
+    # print(game.BoxscoreSummary(gameID).line_score())
     # print(game.BoxscoreSummary(gameID).inactive_players())
     # print(game.BoxscoreSummary(gameID).season_series())
 
