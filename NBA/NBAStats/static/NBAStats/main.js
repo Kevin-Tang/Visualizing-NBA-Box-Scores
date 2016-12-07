dd36cc095925de4f6676d2e0914c46aa29599c53
Kevin Tang <ctang@macalester.edu>
2016-12-07 02:07:30 -0600

    added chart.JS to the static html to port to back end, implemented user design and interface

--- NBA/NBAStats/static/NBAStats/main.js
+++ NBA/NBAStats/static/NBAStats/main.js
@@ -5,3 +5,12 @@ $( function(){
 	});
 	
 });
+
+function populateStatistics(){
+        var stats = document.getElementById("game").innerHTML;
+        var gameDiv = document.createElement("div");
+
+        var text = gameDiv.innerHTML = "Visualization of "+stats+" goes here";
+        document.getElementById("stats").innerHTML = text;
+}
+
