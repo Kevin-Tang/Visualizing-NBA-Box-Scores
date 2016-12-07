e5a24385c374554c86b48de7f47cfe0540d19453
HyunUk Jeong <hjeong326@gmail.com>
2016-12-07 01:18:17 -0600

    commiting

--- nba_py-master/examples/anotherExample.py
+++ nba_py-master/examples/anotherExample.py
@@ -1,15 +1,22 @@
 from __future__ import print_function
 from nba_py import shotchart
 from nba_py import Scoreboard
+from nba_py import game
 
-scoreBoard = Scoreboard()
+
+# scoreBoard = Scoreboard()
+# game_ids_object = scoreBoard.game_header()
+# print(game_ids_object)
 # game_ids_object = scoreBoard.game_header()['GAME_ID']
 # game_ids_str = [ str(item) for item in game_ids_object]
 # print(game_ids_str)
 # print(game_ids_str[3])
-date = scoreBoard._game_date
-print(date)
-print(type(date))
+# date = scoreBoard._game_date
+# print(date)
+# print(type(date))
+
+pbp = game.PlayByPlay('0041400122')
+print(pbp.info())
 
 
 # game_ids = [item['GAME_ID'] for item in s.game_header()]
