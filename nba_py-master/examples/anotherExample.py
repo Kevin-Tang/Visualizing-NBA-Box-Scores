3ca60a1e02fecf54f7e8fba7f6f988a4bdcb48f1
HyunUk Jeong <hjeong326@gmail.com>
2016-12-08 01:26:14 -0600

    can pull quarter scores

--- nba_py-master/examples/anotherExample.py
+++ nba_py-master/examples/anotherExample.py
@@ -1,10 +1,10 @@
 from __future__ import print_function
-from nba_py import shotchart
-from nba_py import Scoreboard
-from nba_py import game
+from nba_py import shotchart, Scoreboard, game
+from datetime import datetime, timedelta
 
+TODAY = datetime.today()
 
-# scoreBoard = Scoreboard()
+# scoreBoard = Scoreboard(TODAY.month, 7)
 # game_ids_object = scoreBoard.game_header()
 # print(game_ids_object)
 # game_ids_object = scoreBoard.game_header()['GAME_ID']
@@ -15,8 +15,23 @@ from nba_py import game
 # print(date)
 # print(type(date))
 
-pbp = game.PlayByPlay('0041400122')
-print(pbp.info())
+# pbp = game.PlayByPlay('0041400122')
+# print(pbp.info())
+
+
+
+summary = game.BoxscoreSummary('0021600322')
+# print(summary.game_summary())
+print ("\n\n\n\n")
+lineScore = summary.line_score()
+print(lineScore)
+print(type(lineScore))
+print ("\n\n\n\n")
+print(lineScore[['PTS_QTR1', 'PTS_QTR2', 'PTS_QTR3', 'PTS_QTR4']])
+print ("\n\n\n\n")
+print(lineScore.get(['PTS_QTR1', 'PTS_QTR2', 'PTS_QTR3', 'PTS_QTR4']))
+
+
 
 
 # game_ids = [item['GAME_ID'] for item in s.game_header()]
---
+++ nba_py-master/examples/tryingStuff.py
@@ -0,0 +1,7 @@
+from __future__ import print_function
+from nba_py import shotchart, Scoreboard, game
+from datetime import datetime, timedelta
+
+TODAY = datetime.today()
+print(TODAY.day)
+
