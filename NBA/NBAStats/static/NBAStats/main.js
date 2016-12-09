18dd8044ac10012c7daad8d04ea7732e3cc168a0
HyunUk Jeong <hjeong326@gmail.com>
2016-12-08 21:08:40 -0600

    working on populating game buttons on the main page

--- NBA/NBAStats/static/NBAStats/main.js
+++ NBA/NBAStats/static/NBAStats/main.js
@@ -1,16 +1,34 @@
 $( function(){
 
-	$( "#datepicker" ).datepicker( {
+	$( "#datepicker" ).datepicker({
 	    onSelect: function(date) {
 	    	month = date.slice(0,2);
 	    	day = date.slice(3,5);
 	    	year = date.slice(6);
 	    	console.log("month is " + month + "\nday is " + day + "\nyear is " + year);
-	    	populateGameButtons();
+	    	populateGameButtons(month, day, year);
 	    }
 	});
+
+	function populateGameButtons(month, day, year){
+		console.log("you called populateGameButtons()");
+		console.log(" the year is " + year);
+		// $.ajax({
+		// 	url : "populate_games/", // the endpoint
+		// 	data : {
+		// 		month : month,
+		// 		day : day,
+		// 		year : year,
+		// 	},
+
+		// 	success : function(json) {
+				
+		// 	}
+		// })
+	}
 	
 });
+
 
 
 
